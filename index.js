1a8e21e6154bf5f4823e9c4e7c030ef88be1c8cf
Pritiranjan <patrapritiranjan11@gmail.com>
2024-05-18 15:38:05 +0530

    age calculator created

---
+++ index.js
@@ -0,0 +1,18 @@
+function calculate(){
+    const dob=document.getElementById("dob").value;
+    const today=new Date();
+    const yourBirthDay=new Date(dob);
+    let currYear=today.getFullYear();
+    let yourBirthYear=yourBirthDay.getFullYear();
+    let age=currYear-yourBirthYear;
+    if(age<0){
+        age=0;
+    }
+
+if(dob==''){
+    document.getElementById("outPut").innerText="Please enter your date of birth";
+}else{
+    document.getElementById("outPut").innerText="You are "+age+" years old";
+}
+
+}
