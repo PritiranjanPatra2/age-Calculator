584776322d9e82abffc686b91802e149ed2e88a3
Pritiranjan <patrapritiranjan11@gmail.com>
2024-05-19 12:58:44 +0530

    background change

--- index.js
+++ index.js
@@ -5,13 +5,16 @@ function calculate(){
     let currYear=today.getFullYear();
     let yourBirthYear=yourBirthDay.getFullYear();
     let age=currYear-yourBirthYear;
-    if(age<0){
-        age=0;
-    }
+    // if(age<0){
+    //     age=0;
+    // }
 
 if(dob==''){
     document.getElementById("outPut").innerText="Please enter your date of birth";
-}else{
+}else if(age<0){
+    document.getElementById("outPut").innerText="Please enter a valid date of birth";
+}
+else {
     document.getElementById("outPut").innerText="You are "+age+" years old";
 }
 
